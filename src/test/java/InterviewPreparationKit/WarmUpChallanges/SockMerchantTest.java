0922758f1d41d112b9f7219123a18bb293e6552b
Farid Najafov <farid.najafov@outlook.com>
2020-03-31 17:47:56 +0400

    -- solved

---
+++ src/test/java/InterviewPreparationKit/WarmUpChallanges/SockMerchantTest.java
@@ -0,0 +1,14 @@
+package InterviewPreparationKit.WarmUpChallanges;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SockMerchantTest {
+
+    @org.junit.jupiter.api.Test
+    void sockMerchant() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
